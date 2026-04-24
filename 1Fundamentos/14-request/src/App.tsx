68f9be7da07f59f1264c9468a54f38ccbb8ffe32
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-24 11:46:11 -0300

    Requisição de outros protocolos

--- 1Fundamentos/14-request/src/App.tsx
+++ 1Fundamentos/14-request/src/App.tsx
@@ -1,9 +1,12 @@
 import UserInfo from "./components/user-info";
+import UserNewForm from "./components/user-new-form";
 
 export function App() {
   return (
     <>
     <UserInfo />
+    <hr />
+    <UserNewForm />
     </>
   )
 }
