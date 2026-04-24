97e9062ddf7b0dc8b18bbe269cb509ea780372a4
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-24 11:28:11 -0300

    Requisição GET manual

---
+++ 1Fundamentos/14-request/src/components/user-info.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import useUser from "../hooks/use-users"
+
+export default function UseInfo() {
+  const { user, userRequestStatus, getUser } = useUser();
+
+  React.useEffect(() => {
+    getUser("gus")
+  }, [getUser])
+
+  if (userRequestStatus === 'loading') {
+    return <div>Carregando usuário...</div>
+  }
+
+  return (
+    <ul>
+      <li>Nome: {user?.name}</li>
+      <li>Username: {user?.id}</li>
+    </ul>
+  )
+}
