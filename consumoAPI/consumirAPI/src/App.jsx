fc1571d1a5a11221c6b5d645c8fa5b62cd54cd79
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-30 14:59:04 -0300

    Consumindo dados de uma API

--- consumoAPI/consumirAPI/src/App.jsx
+++ consumoAPI/consumirAPI/src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 export function App() {
+  // Desestruturação da API
+  const [arrPosts, setArrPosts] = useState([]);
+
+  useEffect(() => {
+    const requisicaoAPI = async () => {
+      const respostaAPI = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
+
+      setArrPosts(respostaAPI.data);
+    };
+
+  requisicaoAPI();    
+  }, []);
+
   return (
-    <h1>Olá, mundo!</h1>
-  )
+    <ul>
+      {arrPosts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
 }
