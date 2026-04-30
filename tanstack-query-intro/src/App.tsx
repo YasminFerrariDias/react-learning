9a3dec5b6afa95421de2a1c9e2b408fc4e5da800
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-30 16:59:29 -0300

    Iniciando o To Do

--- tanstack-query-intro/src/App.tsx
+++ tanstack-query-intro/src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export function App() {
+  const [todoValue, setTodoValue] = useState('')
+
   return (
     <>
-      <h1>Welcome to React Query</h1>
+      <h1>Iniciando no TanStack Query</h1>
+      <div>
+        <input type="text" placeholder="Digite sua tarefa..." />
+        <button type="button">Adicionar</button>
+      </div>
     </>
   )
 }
