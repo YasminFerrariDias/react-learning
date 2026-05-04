6fdb603f41227be94135d082103081f3295d0160
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-04 09:54:17 -0300

    como buscar, exibir, atualizar uma lista

--- tanstack-query-intro/src/App.tsx
+++ tanstack-query-intro/src/App.tsx
@@ -1,15 +1,52 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+type Todo = {
+  id: number;
+  title: string;
+}
 
 export function App() {
   const [todoValue, setTodoValue] = useState('')
+  const [data, setData] = useState<Todo[]>([]);
+
+  async function getTodos() {
+    const { data } = await axios.get<Todo[]>('http://localhost:3000/todos');
+
+    setData(data);
+  }
+
+  async function handleAddTood() {
+    await axios.post('http://localhost:3000/todos', {
+      title: todoValue
+    })
+
+    await getTodos()
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getTodos();
+  }, [])
 
   return (
     <>
       <h1>Iniciando no TanStack Query</h1>
       <div>
-        <input type="text" placeholder="Digite sua tarefa..." />
-        <button type="button">Adicionar</button>
+        <input
+          value={todoValue}
+          onChange={(event) => setTodoValue(event.target.value)}
+          type="text" placeholder="Digite sua tarefa..."
+        />
+        <button type="button" onClick={handleAddTood}>
+          Adicionar
+        </button>
       </div>
+      <ul>
+        {data.map((todo) => (
+          <li key={(todo.id)}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
